4ed3908bbec6d07fa2592db9ecb3673d327329b5
Williamnumismatics <williamnumismatics@gmail.com>
2025-10-17 06:50:17 +1100

    simplified

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def on_tab_e():
 # register chord (works in either order)
 keyboard.add_hotkey('tab+e', on_tab_e)
 
-print("Listening for Tab+E (press Ctrl+C to quit)")
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
+print("Listening")
+while True:
+    if keyboard.
